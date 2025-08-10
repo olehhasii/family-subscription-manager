05dc8c99acd0dbe1a7109264f9fc9415459c92a5
Oleh <olehhasii@gmail.com>
2025-08-10 15:43:03 +0300

    refactor(navbar-island): group children transition with variants

--- src/components/NavBarIsland/NavBarIsland.tsx
+++ src/components/NavBarIsland/NavBarIsland.tsx
@@ -4,54 +4,50 @@ import InfoIcon from '../../icons/InfoIcon';
 import Clock from '../Clock/Clock';
 import DarkThemeIcon from '../../icons/DarkThemeIcon';
 import MoreIcon from '../../icons/MoreIcon';
-import { motion } from 'motion/react';
+import { easeOut, motion } from 'motion/react';
 
 export default function NavBarIsland() {
-  const variants = {
+  const parentVariants = {
+    hidden: {
+      translateX: '-50%',
+      translateY: 100,
+      width: 40,
+      height: 30,
+    },
+    visible: {
+      translateY: 0,
+      width: 360,
+      height: 60,
+      transition: {
+        translateY: { duration: 0.5, type: 'spring', bounce: 0.4 },
+        width: { duration: 0.5, delay: 0.5, ease: 'easeOut' },
+        height: { duration: 0.5, delay: 0.5, ease: 'easeOut' },
+        delayChildren: 0.7,
+      },
+    },
+  };
+
+  const childVariants = {
     visible: {
       scale: 1,
       filter: 'blur(0px)',
       opacity: 1,
+      transition: { ease: easeOut, duration: 0.5 },
     },
     hidden: {
       scale: 0.5,
       filter: 'blur(10px)',
-      opacity: 0,
+      opacity: 0.1,
     },
   };
 
   return (
-    <Wrapper
-      as={motion.nav}
-      initial={{ translateX: '-50%', translateY: 100, width: 40, height: 30 }}
-      animate={{ translateY: 0, width: 360, height: 60 }}
-      transition={{
-        translateY: { duration: 0.5, type: 'spring', bounce: 0.4 },
-        width: { duration: 0.5, delay: 0.5, ease: 'easeOut' },
-        height: { duration: 0.5, delay: 0.5, ease: 'easeOut' },
-      }}
-    >
-      <Logo
-        as={motion.div}
-        initial={{ scale: 0, filter: 'blur(10px)' }}
-        animate={{ scale: 1, filter: 'blur(0px)' }}
-        transition={{ duration: 0.6, delay: 0.7, ease: 'easeOut' }}
-      >
+    <Wrapper as={motion.nav} variants={parentVariants} initial="hidden" animate="visible">
+      <Logo as={motion.div} variants={childVariants}>
         🎧
       </Logo>
-      <Clock
-        variants={variants}
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 0.5, delay: 0.7, ease: 'easeOut' }}
-      />
-      <Actions
-        as={motion.ul}
-        variants={variants}
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 0.5, delay: 0.7, ease: 'easeOut' }}
-      >
+      <Clock variants={childVariants} />
+      <Actions as={motion.ul} variants={childVariants}>
         <ActionButton icon={<DarkThemeIcon width="30px" height="30px" />} />
         <ActionButton icon={<InfoIcon />} />
         <HorizontalLine />
